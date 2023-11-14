06d604c9652ffabc3ae36372a3f470034a34735f
Anthony Anderson <fnanderson93@gmail.com>
2023-11-14 11:05:19 -0500

    fix: :bug: typescript error workaround

--- src/createDefaultTransport.ts
+++ src/createDefaultTransport.ts
@@ -1,8 +1,8 @@
-import { createHeadersInterceptor } from "./createHeadersInterceptor.js";
+import { createHeadersInterceptor, createAuthInterceptor } from "./createHeadersInterceptor.js";
 import { Transport } from "@connectrpc/connect";
 import { createGrpcTransport } from "@connectrpc/connect-node";
 import type { IMessageTypeRegistry } from "@bufbuild/protobuf";
-import { createAuthInterceptor } from "@substreams/core";
+// import { createAuthInterceptor } from "@substreams/core";
 
 export function createDefaultTransport(
   baseUrl: string,
--- src/createHeadersInterceptor.ts
+++ src/createHeadersInterceptor.ts
@@ -9,3 +9,13 @@ export function createHeadersInterceptor(headers?: Headers): Interceptor {
     return next(req);
   };
 }
+
+export function createAuthInterceptor(token: string): Interceptor {
+  return (next) => async (req) => {
+    if (!req.header.has("Authorization")) {
+      req.header.set("Authorization", `Bearer ${token}`);
+    }
+
+    return next(req);
+  };
+}
