4b950f573774011e6cee8df732d1df97e0acf2b2
Anthony Anderson <fnanderson93@gmail.com>
2023-11-15 11:58:20 -0500

    fix: :bug: ugh

--- src/createDefaultTransport.ts
+++ src/createDefaultTransport.ts
@@ -1,15 +1,15 @@
-import { createHeadersInterceptor, createAuthInterceptor } from "./createInterceptors.js";
-import { Transport } from "@connectrpc/connect";
-import { createGrpcTransport } from "@connectrpc/connect-node";
+import { createAuthInterceptor, createHeadersInterceptor } from "./createInterceptors.js";
 import type { IMessageTypeRegistry } from "@bufbuild/protobuf";
+import { Transport } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-node";
 
 export function createDefaultTransport(
   baseUrl: string,
   registry: IMessageTypeRegistry,
   headers?: Headers,
-  token?: string
+  token?: string,
 ): Transport {
-  return createGrpcTransport({
+  return createConnectTransport({
     baseUrl,
     httpVersion: "2",
     interceptors: [createHeadersInterceptor(headers), createAuthInterceptor(token)],
